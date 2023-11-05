d5ece49db09a24d17f7e993b4f5078c7a7efdb76
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-11-05 14:43:47 +0100

    task-2

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -4,6 +4,14 @@ import {
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+import { IconContext } from 'react-icons';
+
+export const StatisticItem = ({ title, total, icon }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ size: '50' }}>{icon}</IconContext.Provider>
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
 };
