533949a9e96fcde63b42d99969f7a9d53b307546
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-05 16:22:43 +0200

    task-4

--- src/utils/dataFormat.js
+++ src/utils/dataFormat.js
@@ -1,6 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
-export const dataForm = (value) => {
-  return formatDistanceToNow(new Date(value), { addSuffix: true })
+export const dataForm = value => {
+  return formatDistanceToNow(new Date(value), { addSuffix: true });
+};
 
-}
+export const formatDate = value => {
+  return format(new Date(value), 'Pp');
+};
